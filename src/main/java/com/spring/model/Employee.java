2ced68674f21ced4e57ebee14e01056314bd09c3
ShubhamAmande <shubhama@mkcl.org>
2023-02-04 16:41:24 +0530

    fc

---
+++ src/main/java/com/spring/model/Employee.java
@@ -0,0 +1,58 @@
+package com.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String dept;
+	private String address;
+	
+	
+	public Employee(int id, String name, String dept, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.address = address;
+	}
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
